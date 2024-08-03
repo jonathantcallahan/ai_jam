7d61a563fb0605bdfa795532a1de3e54f24da0de
Jonathan Callahan <jtc7537@gmail.com>
2024-08-02 20:10:48 -0500

    training environment design

--- AI_Jam/Assets/Scripts/Precursor/ArenaController.cs
+++ AI_Jam/Assets/Scripts/Precursor/ArenaController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 
 public class AreaController : MonoBehaviour
 {
@@ -46,5 +47,35 @@ public class AreaController : MonoBehaviour
             }
         }
         return potentialPosition;
+    }
+
+    private void checkRayCast()
+    {
+        RayPerceptionSensorComponent3D m_rayPerceptionSensorComponent3D = GetComponent<RayPerceptionSensorComponent3D>();
+
+        var rayOutputs = RayPerceptionSensor.Perceive(m_rayPerceptionSensorComponent3D.GetRayPerceptionInput()).RayOutputs;
+        int lengthOfRayOutputs = rayOutputs.Length;
+
+        // Alternating Ray Order: it gives an order of
+        // (0, -delta, delta, -2delta, 2delta, ..., -ndelta, ndelta)
+        // index 0 indicates the center of raycasts
+        for (int i = 0; i < lengthOfRayOutputs; i++)
+        {
+            GameObject goHit = rayOutputs[i].HitGameObject;
+            if (goHit != null)
+            {
+                var rayDirection = rayOutputs[i].EndPositionWorld - rayOutputs[i].StartPositionWorld;
+                var scaledRayLength = rayDirection.magnitude;
+                float rayHitDistance = rayOutputs[i].HitFraction * scaledRayLength;
+
+                // Print info:
+                string dispStr = "";
+                dispStr = dispStr + "__RayPerceptionSensor - HitInfo__:\r\n";
+                dispStr = dispStr + "GameObject name: " + goHit.name + "\r\n";
+                dispStr = dispStr + "Hit distance of Ray: " + rayHitDistance + "\r\n";
+                dispStr = dispStr + "GameObject tag: " + goHit.tag + "\r\n";
+                Debug.Log(dispStr);
+            }
+        }
     }
 }
--- AI_Jam/Assets/Scripts/Precursor/PrecursorAgent.cs
+++ AI_Jam/Assets/Scripts/Precursor/PrecursorAgent.cs
@@ -9,13 +9,14 @@ public class PrecursorAgent : Agent
     public GameObject bullet;
     public GameObject area;
     private AreaController areaController;
+    private int totalScore = 0;
 
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var discreteActionsOut = actionsOut.DiscreteActions;
         var continousActionsOut = actionsOut.ContinuousActions;
-        discreteActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+        //discreteActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
         continousActionsOut[0] = Input.GetAxisRaw("Vertical");
         continousActionsOut[1] = Input.GetAxisRaw("Horizontal");
     }
@@ -31,10 +32,12 @@ public class PrecursorAgent : Agent
         GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * moveForward * Time.deltaTime);
         transform.Rotate(0f, moveRotate, 0f, Space.Self);
         
-        if (discreteActions[0] == 1)
-        {
-            Shoot();
-        }
+        //if (discreteActions[0] == 1)
+        //{
+        //    Shoot();
+        //}
+
+        checkRayCast();
     }
 
     private void Shoot()
@@ -58,5 +61,53 @@ public class PrecursorAgent : Agent
     public override void OnEpisodeBegin()
     {
         EnvironmentReset();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+        Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f);
+        var controller = hit.collider.gameObject;
+        var enemyInSights = controller ? controller.tag == "enemy" : false;
+        sensor.AddObservation(enemyInSights ? 1 : 0);
+        if (enemyInSights) 
+        {
+            AddReward(1);
+            totalScore += 1;
+            if (totalScore > 9)
+            {
+                EndEpisode();
+            }
+        }
+    }
+
+    private void checkRayCast()
+    {
+        RayPerceptionSensorComponent3D m_rayPerceptionSensorComponent3D = GetComponent<RayPerceptionSensorComponent3D>();
+
+        var rayOutputs = RayPerceptionSensor.Perceive(m_rayPerceptionSensorComponent3D.GetRayPerceptionInput()).RayOutputs;
+        int lengthOfRayOutputs = rayOutputs.Length;
+
+        // Alternating Ray Order: it gives an order of
+        // (0, -delta, delta, -2delta, 2delta, ..., -ndelta, ndelta)
+        // index 0 indicates the center of raycasts
+        for (int i = 0; i < lengthOfRayOutputs; i++)
+        {
+            GameObject goHit = rayOutputs[i].HitGameObject;
+            if (goHit != null)
+            {
+                var rayDirection = rayOutputs[i].EndPositionWorld - rayOutputs[i].StartPositionWorld;
+                var scaledRayLength = rayDirection.magnitude;
+                float rayHitDistance = rayOutputs[i].HitFraction * scaledRayLength;
+
+                // Print info:
+                string dispStr = "";
+                dispStr = dispStr + "__RayPerceptionSensor - HitInfo__:\r\n";
+                dispStr = dispStr + "GameObject name: " + goHit.name + "\r\n";
+                dispStr = dispStr + "Hit distance of Ray: " + rayHitDistance + "\r\n";
+                dispStr = dispStr + "GameObject tag: " + goHit.tag + "\r\n";
+                //Debug.Log(dispStr);
+            }
+        }
     }
 }
